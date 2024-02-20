278abd9eea17162d7a5459c8f837cc4db35e92d2
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-02-20 19:01:29 +0530

    Time: 66 ms (14.33%), Space: 49.2 MB (17.63%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -2,16 +2,29 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var sortColors = function(nums) {
-    let n = nums.length;
-    for(let i=0; i<n-1; i++){
-        for(let j=i+1; j<n; j++){
-            let temp =0;
-            if(nums[i]>nums[j]){
-                temp = nums[i]
-                nums[i] = nums[j]
-                nums[j] = temp;
-            }
+var sortColors = function (nums) {
+    const mp = {}
+    let n = nums.length
+
+    for(let i of nums){
+        if(mp[i]){
+            mp[i] += 1
+        }else{
+            mp[i] = 1;
         }
     }
+
+    for(let i=0; i<n; i++){
+        if(mp[0]){
+            nums[i] = 0;
+            mp[0]--
+        }else if(mp[1]){
+            nums[i] = 1;
+            mp[1]--
+        }else if(mp[2]){
+            nums[i] = 2;
+            mp[2]--
+        }
+    }
+
 };
