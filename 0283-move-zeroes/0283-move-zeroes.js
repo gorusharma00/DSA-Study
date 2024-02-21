2fac812d5797e4d7d2de32d0bb2869cc833b374d
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-02-21 13:30:35 +0530

    Time: 409 ms (5.05%), Space: 54.6 MB (14.86%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    for(let i=0;i<nums.length-1;i++){
+        for(let j=i+1; j<nums.length; j++){
+            if(nums[i] == 0){
+                [nums[i], nums[j]] = [nums[j], nums[i]]
+            }
+        }
+    }
+};
