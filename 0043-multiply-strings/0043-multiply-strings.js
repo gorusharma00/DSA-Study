0bc6484e6aca7d4dd10d112de138ee7ed86586de
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-02-22 09:36:21 +0530

    Time: 66 ms (54.29%), Space: 51.5 MB (39.52%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.js
+++ 0043-multiply-strings/0043-multiply-strings.js
@@ -4,9 +4,46 @@
  * @return {string}
  */
 var multiply = function(num1, num2) {
-   
+   /*
    num1 = BigInt(num1)
    num2 = BigInt(num2)
    let product = num1 * num2;
    return BigInt(product).toString();
+   */
+
+    if(!num1 || !num2){       
+        return "0";   
+    } 
+
+   if(num1 === "0" || num2 === "0") return "0";
+
+   let n = num2.length;
+   let m = num1.length;
+   let result = new Array(n+m).fill(0);
+   let pf = 0;
+
+   for(let i=n-1; i>=0; i--){
+       let ival = parseInt(num2[i])
+
+       let k = result.length -1 -pf;
+       let carry = 0;
+       for(let j=m-1; j>=0; j--){
+           let jval = parseInt(num1[j]);
+           let prodcut = ival * jval + carry +  result[k];
+           result[k] = prodcut %10;
+           carry = Math.floor(prodcut /10);
+           k--;
+       }
+       if(carry>0){
+           result[k] += carry;
+       }
+       pf++;
+   }
+    if (result.join('') !== '0') {
+        while (result[0] === 0) {
+            result.shift();
+        }
+    }
+    return result.join('');
+    
 };
