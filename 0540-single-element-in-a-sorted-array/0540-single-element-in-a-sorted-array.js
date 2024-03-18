2f3796562ef1ce0eed750b3f3ab7651b101ab56f
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-03-18 22:46:15 +0530

    Time: 44 ms (96.35%), Space: 52 MB (37.81%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNonDuplicate = function(nums) {
+    let low = 0;
+    let high = nums.length -1;
+    while(low<high){
+        let mid = low + Math.floor((high-low)/2);
+        if(mid % 2 == 1){
+            mid --;
+        }
+        if(nums[mid] == nums[mid+1]){
+            low = mid+2;
+        }else{
+            high = mid;
+        }  
+    }
+    return nums[low];
+}
