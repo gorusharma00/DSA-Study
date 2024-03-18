f0ed698798cfeb68f4d70006b86cfcc20dbbe5ef
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-03-18 15:40:17 +0530

    Time: 89 ms (46.93%), Space: 54 MB (12%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var arrangeCoins = function(n) {
+    let low =1;
+    let high = n;
+    let ans = 1;
+    while(low<=high){
+        let mid = low + Math.floor((high-low)/2);
+        let value = Math.floor(((mid)*(mid+1))/2);
+        if(value<=n){
+            ans = mid;
+            low = mid+1;
+        }else{
+            high = mid-1;
+        }
+    }
+    return ans;
+};
