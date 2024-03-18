f7f5608a3a5eee154b56868cc655bfd9d556499e
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-03-18 12:12:26 +0530

    Time: 54 ms (47.06%), Space: 48.5 MB (86.38%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    let m = matrix.length;
+    let n = matrix[0].length;
+    let low =0;
+    let high = m*n -1;
+    while(low<=high){
+        let mid = low + Math.floor((high-low)/2);
+        let row = Math.floor(mid/n);
+        let col = mid % n;
+        if(matrix[row][col] == target){
+            return true;
+        }else if (matrix[row][col] < target){
+            low = mid+1;
+        }else{
+            high = mid -1;
+        }
+    }
+    return false;
+};
