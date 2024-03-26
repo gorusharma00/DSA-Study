b566fb5a2cc5dfcd2e7a33da09d1c7f5ef54a39f
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-03-26 19:37:52 +0530

    Time: 50 ms (70.26%), Space: 48.6 MB (87.61%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let lo = 0;
+    let hi = nums.length -1;
+    while(lo<=hi){
+        let mid = lo + Math.floor((hi-lo)/2);
+        if(nums[mid] == target){
+            return mid;
+        }
+        if(nums[lo] <= nums[mid]){
+            if(nums[lo] <= target && nums[mid] >= target){
+                hi = mid-1;
+            }else{
+                lo = mid+1;
+            }
+        }else{
+            if(nums[hi] >= target && nums[mid]<=target){
+                lo = mid+1;
+            }else{
+                hi = mid-1;
+            }
+        }
+    }
+    return -1;
+};
