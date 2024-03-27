93e57b6d00f123b6c01608ee91f101d6a3d92009
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-03-27 12:50:23 +0530

    Time: 72 ms (69.6%), Space: 53.2 MB (70.02%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[]} weights
+ * @param {number} days
+ * @return {number}
+ */
+var shipWithinDays = function(weights, days) {
+    let totalWeight = 0;
+    let maxWeight = -1;
+    for(let i=0; i<weights.length; i++){
+        totalWeight += weights[i];
+        maxWeight = Math.max(maxWeight, weights[i]);
+    }
+
+    let lo = maxWeight, hi = totalWeight;
+    let ans = hi;
+    while(lo<=hi){
+        let mid = lo+ Math.floor((hi-lo)/2);
+        if(canShip_WithMidWeight(weights,mid, days)){
+            ans = mid;
+            hi = mid-1;
+        }else{
+            lo = mid+1;
+        }
+    }
+    return ans;
+};
+
+function canShip_WithMidWeight(weights, mid, days){
+    let daysCount = 0;
+    let tw = 0;
+
+    for(let i=0; i<weights.length; i++){
+        if(tw + weights[i] > mid){
+            daysCount++;
+            tw = 0
+        }
+        tw += weights[i];
+    }
+
+    daysCount++;
+    return daysCount <= days;
+}
