39c02be245327f4259f256db83194fff10394096
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-03-27 09:48:14 +0530

    Time: 60 ms (83.51%), Space: 59.1 MB (10.14%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.js
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.js
@@ -16,4 +16,18 @@ var peakIndexInMountainArray = function(arr) {
         }
     }
     return lo;
+
+    /*  ------- this can be another way(increasing graph way) --------
+    let left = 0;
+    let right = arr.length - 1;
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+        if (arr[mid] < arr[mid + 1]) {
+        left = mid + 1;
+        } else {
+        right = mid;
+        }
+    }
+    return left;
+    */
 };
