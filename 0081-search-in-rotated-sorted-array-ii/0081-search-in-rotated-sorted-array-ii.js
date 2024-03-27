c8eab6e842e66545625aa26f9ecd6bb304562081
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-03-27 10:34:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length -1;
+    while(left<=right){
+        let mid = left + Math.floor((right -left)/2);
+        if(nums[mid] == target){
+            return true;
+        }
+        if(nums[mid] >= nums[left]){
+            if(nums[mid] >= target && nums[left] <= target){
+                right = mid -1;
+            }else{
+                left = mid+1;
+            }
+        }else{
+            if(nums[mid] <= target && nums[right] >= target){
+                left = mid+1;
+            }else{
+                right = mid-1;
+            }
+        }
+    }
+    return false;
+};
