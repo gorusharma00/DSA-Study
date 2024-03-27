8d7462f54e0e9fc95d61a012e09481b10a1cc05e
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-03-27 17:55:31 +0530

    Time: 47 ms (85.6%), Space: 49.6 MB (36.4%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum-myapproach.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var splitArray = function(nums, k) {
+    let totalSum = 0;
+    let Max_el = -1
+
+    for(let i=0; i<nums.length; i++){
+        totalSum += nums[i];
+        Max_el = Math.max(Max_el, nums[i])
+    }
+
+    let lo = Max_el
+    let hi = totalSum;
+    let ans = hi;
+
+    while(lo<=hi){
+        let mid = lo + Math.floor((hi-lo)/2);
+        if(maxSum_in_k(nums, mid, k)){
+            ans = mid;
+            hi = mid -1;
+        }else{
+            lo = mid+1
+        }
+    }
+    return ans;
+};
+
+function maxSum_in_k(nums, mid, k){
+    let count = 0;
+    let sum_el= 0
+
+    for(let i=0; i<nums.length; i++){
+        if(sum_el + nums[i] > mid){
+            count++;
+            sum_el = 0
+        }
+        sum_el += nums[i]
+    }
+    count++;
+    return count <= k;
+}
