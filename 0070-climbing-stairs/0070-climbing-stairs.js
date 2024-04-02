16b5a5204fd8e7d11969d8747e8af1301d2172ef
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-04-02 22:21:17 +0530

    Time: 48 ms (72.7%), Space: 48.9 MB (31.93%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -4,19 +4,17 @@
  */
 let dp;
 var climbStairs = function(n) {
-    dp = new Array(50).fill(-1);
-    return f(n);
+    return f_bu(n);
 };
 
-function f(n){
-    if( n< 0) return 0;
-    if(n == 0 || n == 1){
-        return 1;
+function f_bu(n){
+    dp = new Array(50);
+    dp[0] = 1;
+    dp[1] = 1;
+    
+    for(let i = 2; i<= n; i++){
+        dp[i] = dp[i - 1] + dp[i -2];
     }
 
-    if(dp[n] != -1) return dp[n]
-
-    return dp[n] = f(n-1) + f(n-2);
+    return dp[n]
 }
-
-//time complexity => O(n)
