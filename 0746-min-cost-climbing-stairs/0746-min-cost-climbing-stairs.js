c5e5f060c7be725d4dbf60a95634e1c88f1388c1
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-04-03 09:58:56 +0530

    Time: 55 ms (68.25%), Space: 51 MB (13.14%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -5,7 +5,7 @@
 
 let c;
 let dp;
-var minCostClimbingStairs = function(cost) {
+var minCostClimbingStairs = function (cost) {
     dp = Array(1005).fill(-1);
 
     c = cost
@@ -15,14 +15,14 @@ var minCostClimbingStairs = function(cost) {
     return Math.min(first, second);
 };
 
-function f(i){
-   if(i>=c.length){
-        return 0; 
-   }
-   if(dp[i] != -1) return dp[i];
-   
-   let first = c[i] + f(i+1)
-   let second = c[i] + f(i+2)
+function f(i) {
+    if (i >= c.length) {
+        return 0;
+    }
+    if (dp[i] != -1) return dp[i];
 
-   return Math.min(first, second);
+    let first = c[i] + f(i + 1)
+    let second = c[i] + f(i + 2)
+
+    return dp[i] = Math.min(first, second);
 }
