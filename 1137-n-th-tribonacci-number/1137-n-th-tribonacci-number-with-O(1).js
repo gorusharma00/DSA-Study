43a508333977d0cdd086f863f9e1c1301e745920
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-04-04 22:17:21 +0530

    Time: 62 ms (6.2%), Space: 48.5 MB (66.67%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number-with-O(1).js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var tribonacci = function(n) {
+    if(n <= 2){
+        return (n == 0) ? 0 : 1;
+    }
+
+    let t0 = 0, t1 = 1, t2 = 1;
+
+    for(let i = 3; i<=n; i++){
+        let ans = t0 + t1 + t2;
+         
+         t0 = t1;
+         t1 = t2;
+         t2 = ans;
+
+    }
+    return t2;
+};
