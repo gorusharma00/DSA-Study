45cbf92d09b43a90e1864c5bc638db032cedbd2e
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-04-05 17:26:17 +0530

    Time: 45 ms (97.41%), Space: 48.4 MB (97.98%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv-top-down.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+let n;
+let dp;
+var combinationSum4 = function(nums, target) {
+    dp = Array(1005).fill(-1);
+    n = nums;
+    return f(target)
+};
+
+function f(target){
+
+    if(target == 0) return 1;
+
+    if(dp[target] != -1) return dp[target];
+    
+    let ans = 0;
+
+    for(let i = 0; i< n.length; i++){
+        if(target - n[i] >= 0){
+            ans += f(target - n[i]);
+        }
+    }
+
+    return dp[target] = ans;
+}
