7c118242c4d24be8a921a973b9ed0da4a89db960
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-04-10 10:27:33 +0530

    Time: 52 ms (67.09%), Space: 50.5 MB (10.76%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.js
+++ 0413-arithmetic-slices/0413-arithmetic-slices.js
@@ -2,26 +2,27 @@
  * @param {number[]} nums
  * @return {number}
  */
-let arr;
-let dp = new Array(5005);
+ let arr;
 var numberOfArithmeticSlices = function(nums) {
     arr = nums;
-    dp.fill(-1)
-    let ans = 0;
-    for(let i = 0; i<nums.length; i++){
-         ans += f(i);
-    }
-    return ans;
+    return f_bu()
 };
 
-function f(i){
-    if(i == 0 || i == 1) return 0;
-
-    if(dp[i] != -1)return dp[i];
-
-    if(arr[i] - arr[i-1] != arr[i-1] - arr[i-2]){
-        return dp[i] = 0;
-    }else{
-        return dp[i] = 1 + f(i-1);
+function f_bu(){
+    let dp = new Array(5005);
+    for(let i = 0; i< arr.length; i++){
+        if(i == 0 || i ==1) dp[i] = 0;
+        else{
+            if(arr[i] - arr[i-1] != arr[i-1] - arr[i-2]){
+                dp[i] = 0;
+            }else{
+                dp[i] = 1 + dp[i-1];
+            }
+        }
     }
+    let ans = 0;
+    for(let i = 0; i< arr.length; i++){
+        ans+= dp[i];
+    }
+    return ans;
 }
