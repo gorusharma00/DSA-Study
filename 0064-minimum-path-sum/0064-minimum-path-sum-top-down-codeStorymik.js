e1f01386c0610e8e9b9fafb323489612d70880d3
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-04-11 18:05:17 +0530

    Time: 39 ms (99.77%), Space: 55.7 MB (14.67%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum-top-down-codeStorymik.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+let matrix;
+let dp;
+var minPathSum = function(grid) {
+    matrix = grid;
+    dp = new Array(grid.length+1);
+    for(let i = 0; i<grid.length+1; i++){
+        dp[i] = new Array(2005).fill(-1);
+    }
+    return f(0,0);
+};
+
+function f(row, col){
+    let n = matrix.length;
+    let m = matrix[0].length;
+
+    if(dp[row][col] != -1) return dp[row][col];
+
+    if(row == n-1 && col == m-1) return matrix[row][col];
+
+    if(row == n-1){
+        return dp[row][col] = matrix[row][col] + f(row, col+1);
+    }else if(col == m-1){
+         return dp[row][col] = matrix[row][col] + f(row+1, col);
+    }else{
+         return dp[row][col] = matrix[row][col] + Math.min(f(row+1, col), f(row, col+1));
+    }
+}
