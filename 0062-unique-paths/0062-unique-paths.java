5019a97791b04901e73722aef74f496c02df8012
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-04-15 16:08:51 +0530

    Time: 2 ms (11.63%), Space: 43.1 MB (5.04%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,28 @@
+class Solution {
+    public static int[][] dp;
+    public static int[][] matrix;
+
+    public static int f(int i, int j){
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        if(i >= m || j >= n) return 0;
+        if(i == m-1 && j == n-1) return 1;
+
+        if(dp[i][j] != 0) return dp[i][j];
+
+        return dp[i][j] = f(i+1, j) + f(i, j+1);
+        
+    }
+
+    public int uniquePaths(int m, int n) {
+        matrix = new int[m][n];
+        dp = new int[105][105];
+
+        for(int i = 0; i< 105; i++){
+            Arrays.fill(dp[i], 0);
+        }
+
+        return f(0,0);
+    }
+}
