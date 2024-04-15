83be3428976b5a7d431e9acf212edfb989932126
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-04-15 22:18:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,36 +1,42 @@
 class Solution {
-    public static int[][] dp = new int[105][105];
+    public static int[][] dp;
     public static int[][] matrix;
 
-    public static int f(int i, int j){
+    public static int f_bu(){
+        dp = new int[105][105];
         int m = matrix.length;
         int n = matrix[0].length;
 
-        if(i >= m || j>= n) return 0;
-        if(i == m-1 && j == n-1 && matrix[m-1][n-1] != 1) return 1;
+        if(matrix[0][0] == 1 || matrix[m-1][n-1] == 1) return 0;
 
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(matrix[i][j] == 1){
-            return dp[i][j] = 0;
+        // first row
+        for(int j = 0; j <= n-1; j++){
+            if(matrix[0][j] == 1) continue;
+            dp[0][j] = 1;
         }
-        return dp[i][j] = f(i+1, j) + f(i, j+1);
+
+        // first column
+        for(int i = 0; i <= m-1; i++){
+            if(matrix[i][0] == 1) continue;
+            dp[i][0] = 1;
+        }
+
+        for(int i = 1; i<= m-1; i++){
+            for(int j = 1; j<= n-1; j++){
+                 if (matrix[i][j] == 1) {
+                    dp[i][j] = 0; // Set to 0 if there's an obstacle
+                } else {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         matrix = obstacleGrid;
 
-        for(int i = 0; i< 105; i++){
-            Arrays.fill(dp[i], -1);
-        }
-
-        return f(0,0);
+        return f_bu();
     }
 }
-
-// time complexity => O(m * n)
-/*
-space complexity => O(105 * 105) as implemented, 
-                    but more dynamically it would be O(m * n) for the memoization table plus a 
-                    smaller term for the recursive call stack depth, which is often omitted in big O notation.
-*/
