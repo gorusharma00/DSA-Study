fa3ff440caf82004dc5af76d568e33d6b4c3a7b6
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-04-16 18:47:53 +0530

    Time: 22 ms (46.61%), Space: 44.9 MB (94.43%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,26 +1,28 @@
 class Solution {
-    public static int[][] dp = new int[1005][1005];
-    public static int f(String s1, String s2, int i , int j){
+    public static int[] prev;
+    public static int[] curr ;
 
-        if(i == -1 || j== -1) return 0; // if any one of string is exhausted return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(s1.charAt(i) == s2.charAt(j)){
-            return dp[i][j] = 1 + f(s1, s2, i -1, j - 1);
-        }else{
-            return dp[i][j] = Math.max(f(s1, s2, i-1, j), f(s1, s2, i, j-1));
-        }
-
-    }
-    public int longestCommonSubsequence(String text1, String text2) {
-
-        for(int i = 0; i<1005; i++){
-            for(int j = 0; j < 1005; j++){
-                dp[i][j] = -1;
+    public static int f_bu(String s1, String s2){
+        prev = new int[s2.length() + 1];
+        curr = new int[s2.length() + 1];
+        // changing indexing to 1based for base case
+        for(int i = 1; i <= s1.length(); i++){
+            for(int j = 1;  j <= s2.length(); j++){
+                // if(i == 0 || j == 0) curr[j] = 0;
+                
+                    if(s1.charAt(i-1) == s2.charAt(j-1)){
+                        curr[j] = 1 + prev[j - 1];
+                    }else{
+                        curr[j] = Math.max(prev[j], curr[j - 1]);
+                    }
+                
             }
+            prev = curr.clone();
         }
-        
-        return f(text1, text2, text1.length() - 1, text2.length() -1);
+        return prev[s2.length()];
+    }
+
+    public int longestCommonSubsequence(String text1, String text2) {    
+        return f_bu(text1, text2);
     }
 }
