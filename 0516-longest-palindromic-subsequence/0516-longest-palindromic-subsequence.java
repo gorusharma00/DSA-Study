a8f7a2a0790b07b2b4cf26b031e6299fb5816435
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-04-17 12:11:51 +0530

    Time: 51 ms (48.43%), Space: 46.6 MB (90.98%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,29 +1,27 @@
 class Solution {
-    public static String word;
-    public static int[][] dp;
+    public static int[][] dp = new int[1005][1005];
 
-    public static int f(int i, int j){
-        if(i > j) return 0;
-        if(i == j) return 1;
+    public static int f(String s, int i, int j){
+        if(i == j) return 1;  // longest palindromic subsequence of single char is that char only
+
+        if( i > j) return 0; 
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        int ans = 0;
-        if(word.charAt(i) == word.charAt(j)){
-            ans = 2 + f(i+1, j-1);
+        if(s.charAt(i) == s.charAt(j)){
+            return dp[i][j] = 2 + f(s, i+1, j-1);
         }else{
-           ans = Math.max(f(i+1,j), f(i,j-1));
+            return dp[i][j] = Math.max(f(s, i + 1, j), f(s, i, j - 1));
         }
-        return dp[i][j] = ans;
     }
 
     public int longestPalindromeSubseq(String s) {
-        word = s;
-        dp = new int[1005][1005];
-        for(int i = 0; i< 1005; i++){
-            Arrays.fill(dp[i], -1);
+        for(int i = 0; i < 1005; i++){
+            for(int j = 0; j < 1005; j++){
+                dp[i][j] = -1;
+            }
         }
 
-        return f(0, s.length()-1);
+        return f(s, 0, s.length()-1);
     }
 }
