cd20c749cf9f100badcdcc568a0d2ab62f99e3cc
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-04-20 17:55:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0895-maximum-frequency-stack/0895-maximum-frequency-stack.java
@@ -0,0 +1,49 @@
+class FreqStack {
+        private HashMap<Integer, Integer> freqMap;
+        private int maxFreq;
+        private HashMap<Integer, ArrayList<Integer>> elementMap;
+
+    public FreqStack() {
+        freqMap = new HashMap<>();
+        maxFreq = 0;
+        elementMap = new HashMap<>();
+    }
+    
+    public void push(int val) {
+        if(!freqMap.containsKey(val)){
+            freqMap.put(val, 1);
+        }else{
+            int freq = freqMap.get(val);
+            freqMap.put(val, freq+1);
+        }
+
+        maxFreq = Math.max(maxFreq, freqMap.get(val));
+
+        if(!elementMap.containsKey(freqMap.get(val))){
+            elementMap.put(freqMap.get(val), new ArrayList<>());
+            elementMap.get(freqMap.get(val)).add(val);
+        }else{
+            elementMap.get(freqMap.get(val)).add(val);
+        }
+    }
+    
+    public int pop() {
+        ArrayList<Integer> arr = elementMap.get(maxFreq);
+        int result = arr.get(arr.size() - 1);
+        arr.remove(arr.size() - 1);
+
+        if(arr.isEmpty()){
+            elementMap.remove(maxFreq);
+        
+            maxFreq--;
+        }
+        return result;
+    }
+}
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack obj = new FreqStack();
+ * obj.push(val);
+ * int param_2 = obj.pop();
+ */
