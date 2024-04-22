e8f201f71aa864981db3033f9cfe39e0b1436afe
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-04-22 13:23:32 +0530

    Time: 58 ms (40%), Space: 48.6 MB (94.17%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.js
+++ 0844-backspace-string-compare/0844-backspace-string-compare.js
@@ -3,62 +3,24 @@
  * @param {string} t
  * @return {boolean}
  */
-class Stack{
-    #arr;
-
-    constructor(){
-        this.#arr = [];
-    }
-
-    push(val){
-        this.#arr.push(val);
-    }
-
-    pop(){
-        this.#arr.pop();
-    }
-
-    isEmpty(){
-        return this.#arr.length == 0;
-    }
-
-    top(){
-        return this.#arr[this.#arr.length -1];
-    }
-}
-
 var backspaceCompare = function(s, t) {
-    let s1 = new Stack();
-    let s2 = new Stack();
+    let s1 = updateString(s);
+    let s2 = updateString(t);
 
-    for(let i = 0; i < s.length; i++){
-        if(!s1.isEmpty() && s[i] == "#"){
-            s1.pop();
-        }
-        else{
-            if(s[i] != "#")
-            s1.push(s[i]);
-        }
-
-    }
-
-    for(let i = 0; i < t.length; i++){
-        if(!s2.isEmpty() && t[i] == "#"){
-            s2.pop();
-        }else{
-            if(t[i] != "#")
-            s2.push(t[i]);
-        }
-
-    }
-
-    while(!s1.isEmpty() && !s2.isEmpty()){
-        if(s1.top() != s2.top()){
-            return false;
-        }
-        s1.pop();
-        s2.pop();
-    }
-
-    return s1.isEmpty() && s2.isEmpty();
+    return s1 == s2;
 };
+
+function updateString(s){
+    let newString = "";
+     let skip = 0; // To keep track of backspaces
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (s[i] === '#') {
+            skip++;
+        } else if (skip > 0) {
+            skip--;
+        } else {
+            newString = s[i] + newString;
+        }
+    }
+    return newString;
+}
