5046037dd523f2a251be8dda1f44163f2311e44c
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-04-27 06:20:39 +0530

    Time: 48 ms (74.71%), Space: 48.7 MB (54.56%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks-enqueue-efficient.js
@@ -0,0 +1,50 @@
+var MyQueue = function() {
+    this.stack = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.stack.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    let copy = [];
+    while(this.stack.length != 0){
+        copy.push(this.stack.pop())
+    }
+    let element = copy.pop();
+
+    while(copy.length != 0){
+        this.stack.push(copy.pop());
+    }
+    return element;
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    return this.stack[0];
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return this.stack.length == 0;
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
