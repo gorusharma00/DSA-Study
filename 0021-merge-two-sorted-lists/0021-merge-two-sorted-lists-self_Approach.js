5a8516acef544b107d119ae898e3d5b429f4725a
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-04-30 12:47:11 +0530

    Time: 77 ms (5.49%), Space: 51.3 MB (77.84%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists-self_Approach.js
@@ -0,0 +1,66 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+    if(list1 == null && list2 == null) return null;
+
+    if(list1 == null) return list2;
+    if(list2 == null) return list1;
+
+    let temp1 = list1;
+    let temp2 = list2;
+
+    let dummyHead = null;
+    let curr = dummyHead;
+
+    while(temp1 != null && temp2 != null){
+        if(temp1.val < temp2.val){
+            if(curr == null){
+                dummyHead = new ListNode(temp1.val);
+                curr = dummyHead;
+            }else{
+                curr.next = new ListNode(temp1.val);
+                curr = curr.next;
+            }
+            temp1 = temp1.next;
+        } else{
+            if(curr == null){
+                dummyHead = new ListNode(temp2.val);
+                curr = dummyHead;
+            }else{
+                curr.next = new ListNode(temp2.val);
+                curr = curr.next;
+            }
+
+            temp2 = temp2.next;
+        }
+        
+    }
+
+    while(temp1 != null){
+        curr.next = new ListNode(temp1.val);
+        curr = curr.next;
+        temp1 = temp1.next;
+    }
+
+    while(temp2 != null){
+        curr.next = new ListNode(temp2.val);
+        curr = curr.next;
+        temp2 = temp2.next;
+    }
+
+    return dummyHead;
+};
+
+// Time Complexity: O(n + m)
+
+// Space Complexity: O(n + m)
