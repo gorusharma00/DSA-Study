9c005edb3fa1680ad5e6ba6a822a634587399575
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-05-08 16:18:33 +0530

    Time: 65 ms (61.98%), Space: 57.4 MB (32.7%) - LeetHub

---
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value-Approach_2.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findBottomLeftValue = function(root) {
+    let qu = [];
+    qu.push(root);
+    // qu.push(null); // no need to put marker
+
+    let leftMost;
+
+    while(qu.length > 0){
+        let curr = qu.shift();
+
+        leftMost = curr.val;
+        if(curr.right){
+            qu.push(curr.right);
+        }
+
+        if(curr.left){
+            qu.push(curr.left);
+        }
+    }
+    return leftMost;
+};
