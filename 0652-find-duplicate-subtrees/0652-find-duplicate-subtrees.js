f3a725983c29be05dfb68b6839f78608b2799f0b
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-05-10 08:44:48 +0530

    Time: 93 ms (54.54%), Space: 63.5 MB (33.12%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.js
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.js
@@ -10,24 +10,26 @@
  * @param {TreeNode} root
  * @return {TreeNode[]}
  */
-let mp = {};
-let result = [];
-function f(root) {
-    if(root == null) return '#';
-    
+let mp;
+let result;
+function f(root){
+    if(root == null) return "#";
+
     const left = f(root.left);
     const right = f(root.right);
-    
-    const s = "" + root.val + "," + left + "," + right;
-    if(!mp[s]) {
-        mp[s] = 1
-    } else {
+
+    let s= root.val + "," + left + "," + right;
+
+    if(!mp[s]){
+        mp[s] = 1;
+    }else{
         if(mp[s] == 1) {
             // this is the first time we detected a duplicate
             result.push(root);
         }
         mp[s] += 1;
     }
+
     return s;
 }
 var findDuplicateSubtrees = function(root) {
