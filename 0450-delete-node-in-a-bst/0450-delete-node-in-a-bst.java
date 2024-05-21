4a1ec82aec8d4ec7a1d346d31f1c07b1ae7f9c43
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-05-21 09:46:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public static TreeNode f(TreeNode root, int key){
+        if(root == null) return null;
+
+        if(root.val > key){
+            // go in left;
+            root.left = f(root.left, key);
+        }else if(root.val < key){
+            // go in right;
+            root.right = f(root.right, key);
+        }else {
+            // where we find root.val == key, that is we have to delete
+
+            if(root.left == null && root.right == null){
+                // case where we find leaf node
+                return null;
+            }else if(root.left == null){
+                return root.right;
+                // case where subtree has only one child
+            }else {
+                // case where subtress has both child
+
+                TreeNode temp = root.right;
+                while(temp != null && temp.left != null){
+                    temp = temp.left;
+                }
+
+                root.val = temp.val;
+
+                root.right = f(root.right, temp.val);
+            }
+        }
+
+        return root;
+    }
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        return f(root, key);
+    }
+}
