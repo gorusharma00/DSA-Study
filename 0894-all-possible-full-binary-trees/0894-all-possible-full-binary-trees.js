0d9f36a0ddf5d3d38a2f83a2b10a5967defaa4ae
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-05-23 20:23:15 +0530

    Time: 99 ms (86.67%), Space: 60.7 MB (57.78%) - LeetHub

---
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number} n
+ * @return {TreeNode[]}
+ */
+ let map;
+function f(n){
+    if(n % 2 == 0) return []; // because only odd can make full BT
+    if(n == 1){
+        let root = new TreeNode(0);
+        return [root];
+    }
+
+    if(map.has(n)){
+        return map.get(n);
+    }
+
+    let result = [];
+    for(let i = 1; i < n; i += 2){
+        // incremented by 2 because we need odd i
+
+        let leftAllFBT = f(i);
+        let rightAllFBT = f(n - i -1);
+
+        // now pairing
+        for(let l of leftAllFBT){
+            for(let r of rightAllFBT){
+                let root = new TreeNode(0);
+                root.left = l;
+                root.right = r;
+                result.push(root);
+            }
+        }
+    }
+    map.set(n, result);
+    return result;
+}
+
+var allPossibleFBT = function(n) {
+    map = new Map();
+    return f(n);
+};
