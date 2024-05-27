486b0b9107e4b9d763ce6807e2c0b8e391369cef
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-05-27 12:04:49 +0530

    Time: 223 ms (40%), Space: 61.5 MB (40%) - LeetHub

--- 1569-number-of-ways-to-reorder-array-to-get-same-bst/1569-number-of-ways-to-reorder-array-to-get-same-bst.js
+++ 1569-number-of-ways-to-reorder-array-to-get-same-bst/1569-number-of-ways-to-reorder-array-to-get-same-bst.js
@@ -3,11 +3,21 @@
  * @return {number}
  */
 let mod = BigInt(1e9 + 7);
-let pascal_tri;
+function factorial(n){
+    if(n == 0 || n == 1){
+        return 1n;
+    }
+
+    let result = 1n;
+    for(let i = 2; i <= n; i++){
+        result *= BigInt(i);
+    }
+    return result;
+}
 
 function f(nums){
     let n = nums.length;
-    if(n < 3) return 1; // edge case
+    if(n < 3) return 1n; // edge case
 
     let left_arr = [];
     let right_arr = [];
@@ -24,25 +34,16 @@ function f(nums){
 
     let x = BigInt(f(left_arr));
     let y = BigInt(f(right_arr));
-    let z = BigInt(pascal_tri[n-1][left_arr.length]);      // nCr => (n-1)C(left_arr.size) or it can be right_arr.size also
+    let z = (factorial(n -1) / (factorial(left_arr.length) * factorial(n - 1 - left_arr.length))) % mod;      // nCr => (n-1)C(left_arr.size) or it can be right_arr.size also
 
-    return (((x * y) % mod * z) ) % mod;
+    return (((x * y) % mod * z) % mod);
 }
 
 var numOfWays = function(nums) {
-    let n = nums.length;
-    pascal_tri = new Array(n);
-
-    for(let i = 0; i < n; i++){
-        pascal_tri[i] = new Array( i + 1).fill(1n);
-
-        for(let j = 1; j < i; j++){
-            // j starts from 1 as in PT one col will be 1 same with j < 1
-            pascal_tri[i][j] = (pascal_tri[i-1][j] + pascal_tri[i-1][j-1]) % mod; // this one is diagonally
-        }
-    }
-
     let ans = BigInt(f(nums));
 
     return (ans - 1n) % mod;  // subtract 1 because exclude the orignal arrangement
 };
+
+// TC => o(n * n) in worst case depth of tree will be of n size and we also run loop for n times
+// SC => o(n * n) // 2d pascal aray
