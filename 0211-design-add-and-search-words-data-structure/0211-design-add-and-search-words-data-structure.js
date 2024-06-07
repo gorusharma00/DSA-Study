39a3a09e176dd258aac05d2dde09b1cb699f3244
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-06-07 08:13:37 +0530

    Time: 1032 ms (53.2%), Space: 93.2 MB (44.79%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.js
@@ -0,0 +1,71 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.isTerminal = false; 
+        this.children = {}
+    }
+}
+
+var WordDictionary = function() {
+     this.root = new Node(null);
+};
+
+/** 
+ * @param {string} word
+ * @return {void}
+ */
+WordDictionary.prototype.addWord = function(word) {
+     let curr = this.root;
+    for(let i = 0; i < word.length; i++) {
+        let ch = word[i];
+        if(curr.children[ch]) {
+            // if the child is present
+            curr = curr.children[ch];
+        } else {
+            let n = new Node(ch);
+            curr.children[ch] = n;
+             curr = curr.children[ch];
+        }
+    }
+    // when the loop ends curr is at the node of last char
+    curr.isTerminal = true;
+};
+
+/** 
+ * @param {string} word
+ * @return {boolean}
+ */
+function searchUtil(root, word){
+    let curr = root;
+    for(let i = 0; i < word.length; i++) {
+        let ch = word[i];
+        if(ch == "."){
+            for(let j = 0; j < 26; j++){
+                if(curr.children[String.fromCharCode(97 + j)] != null){
+                    if(searchUtil(curr.children[String.fromCharCode(97 + j)], word.substring(i+1))){
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        else if(curr.children[ch]) {
+            curr = curr.children[ch];
+        } else {
+            return false;
+        }
+    }
+
+    return curr.isTerminal;
+}
+
+WordDictionary.prototype.search = function(word) {
+    return searchUtil(this.root, word);
+};
+
+/** 
+ * Your WordDictionary object will be instantiated and called as such:
+ * var obj = new WordDictionary()
+ * obj.addWord(word)
+ * var param_2 = obj.search(word)
+ */
