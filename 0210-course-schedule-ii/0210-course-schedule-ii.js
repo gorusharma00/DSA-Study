b7e775d79ddfe5ffca27e786dc97d8cbfa86fee0
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-06-28 16:32:17 +0530

    Time: 83 ms (35.25%), Space: 55.9 MB (51.62%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.js
@@ -0,0 +1,53 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {number[]}
+ */
+function topoSort(g, V, indegree){
+    let qu = [];
+    let visited = new Set();
+
+    for(let i = 0; i < V; i++){  // v is total no of vertices
+        if(indegree[i] == 0) {
+            qu.push(i);
+            visited.add(i);
+        }
+    }
+
+    let count = 0;
+    let result = [];
+    while(qu.length !== 0){
+        let curr = qu.shift();
+        result.push(curr);
+        count++;
+
+        for(let neigbour of g[curr]){
+            if(!visited[neigbour]){
+                indegree[neigbour]--;   
+
+                if(indegree[neigbour] == 0){
+                    qu.push(neigbour); // add those nodes whose indegree become 0
+                }
+            }
+        }
+    }
+
+    return count === V ? result : [];
+}
+
+var findOrder = function(numCourses, prerequisites) {
+    let g = new Array(numCourses); // graph
+    for(let i = 0; i < numCourses; i++) g[i] = new Array();
+    
+    let indegree = new Array(numCourses).fill(0);  // to keep the track of incoming nodes on that index
+
+    for(let i = 0; i < prerequisites.length; i++){
+        let curr = prerequisites[i];
+        let a = curr[0];
+        let b = curr[1];
+        g[b].push(a);
+        indegree[a]++;
+    }
+
+    return topoSort(g, numCourses, indegree);
+};
